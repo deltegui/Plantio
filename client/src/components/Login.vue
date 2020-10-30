1fbb2afbb66b1e446435709e8155d58fe5ce02e6
deltegui <deltegui@hotmail.com>
2020-10-30 21:22:57 +0100

    push a real server

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="login">
-    <span>Usuario</span>
-    <input class="field" type="text"/>
-    <span>Password</span>
-    <input class="field" type="password"/>
-    <button class="field fynd-btn">Login</button>
+    <label for="username">Usuario</label>
+    <input v-model="username" id="username" class="field" type="text"/>
+    <label for="password">Password</label>
+    <input v-model="password" id="password" class="field" type="password"/>
+    <button v-on:click="doLogin" class="field fynd-btn">Login</button>
     <button class="field fynd-btn">Register</button>
   </div>
 </template>
 
 <script>
+// import store from '../store';
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    doLogin() {
+    },
+  },
 };
 </script>
 
 <style scoped>
-span {
+label {
   display: block;
   margin-bottom: 10px;
 }
@@ -26,19 +38,23 @@ span {
   margin-left: auto;
   margin-right: auto;
   padding-top: 40px;
+
+  --bg-color: #353535;
+  --front-color: #00d1b2;
+  --danger-color: #b31d12;
 }
 
 input {
   outline: none;
   -webkit-box-flex: 18;
-          flex-grow: 18;
+  flex-grow: 18;
   border-width: 0px;
   font-size: 20px;
 
   padding-left: 7px;
-  background-color: #353535;
+  background-color: var(--bg-color);
   color: white;
-  border-color: #353535;
+  border-color: var(--bg-color);
 }
 
 .field {
@@ -49,13 +65,12 @@ input {
 
   border-style: solid;
   border-width: 1px;
-  border-color: #353535;
+  border-color: var(--bg-color);
   border-radius: 5px 5px 5px 5px;
 
   box-shadow: 2px 2px 1px black;
   box-sizing: border-box;
 
-  display: -webkit-box;
   display: flex;
 
   font-size: 14px;
@@ -67,7 +82,7 @@ input {
 
 .field:hover,
 .field:focus-within {
-  border-color: #00d1b2;
+  border-color: var(--front-color);
 }
 
 .fynd-btn {
@@ -76,7 +91,7 @@ input {
   border-style: solid;
   border-radius: 10px 10px 10px 10px;
   border-width: 1px;
-  border-color: #00d1b2;
+  border-color: var(--front-color);
 
   background-color: #252525;
   color: white;
@@ -90,14 +105,14 @@ input {
 }
 
 .fynd-btn:hover {
-  background-color: #00d1b2;
+  background-color: var(--front-color);
 }
 
 .danger-btn {
-  border-color: #b31d12;
+  border-color: var(--danger-color);
 }
 
 .danger-btn:hover {
-  background-color: #b31d12;
+  background-color: var(--danger-color);
 }
 </style>
---
+++ client/tests/unit/game/coordinate.spec.js
@@ -0,0 +1,11 @@
+import CropCoordinate from '../../../src/game/coordinate';
+
+describe('CropCoordinate', () => {
+  describe('toRealPosition', () => {
+    it('should transform a cropcoordinate to real pixel position', () => {
+      const coordinate = new CropCoordinate(1, 3);
+      const expectedPosition = { x: 0, y: 0 };
+      expect(coordinate.toRealPosition()).toBeEql(expectedPosition);
+    })
+  });
+});
